1a8415960cd337fea14b2437718c92772793287c
Marco Martino <59832916+Marco-Maker@users.noreply.github.com>
2025-04-19 11:33:50 +0200

    Aggiunto groq a crew ai

--- language_learning_mentor/crew.py
+++ language_learning_mentor/crew.py
@@ -2,35 +2,53 @@ from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 from tools.email_sender import EmailSender
 from tools.calculator import QuizCalculator
+from pathlib import Path
+from groq import Groq
+from langchain_groq import ChatGroq
+import os, yaml
+from dotenv import load_dotenv
 
 
 @CrewBase
 class LanguageMentor():
     """Language Learning Mentor Crew"""
+    load_dotenv() 
+    agents_config = yaml.safe_load(Path('config/agents.yaml').read_text())
+    tasks_config = yaml.safe_load(Path('config/tasks.yaml').read_text())
 
-    agents_config = 'config/agents.yaml'
-    tasks_config = 'config/tasks.yaml'
+    def _make_groq_llm(self, cfg: dict) -> ChatGroq:
+        return ChatGroq(
+            model=cfg['model_name'],
+            temperature=cfg.get('temperature', 0.0),
+            api_key=os.getenv("GROQ_KEY")
+        )
 
     @agent
     def level_detector(self) -> Agent:
+        cfg = self.agents_config['level_detector']
+        llm = self._make_groq_llm(cfg)
         return Agent(
-            config=self.agents_config['level_detector'],
+            llm=llm,
             tools=[QuizCalculator()],
             verbose=True
         )
 
     @agent
     def tip_agent(self) -> Agent:
+        cfg = self.agents_config['tip_agent']
+        llm = self._make_groq_llm(cfg)
         return Agent(
-            config=self.agents_config['tip_agent'],
+            llm=llm,
             tools=[EmailSender()],
             verbose=True
         )
 
     @agent
     def quiz_agent(self) -> Agent:
+        cfg = self.agents_config['quiz_agent']
+        llm = self._make_groq_llm(cfg)
         return Agent(
-            config=self.agents_config['quiz_agent'],
+            llm=llm,
             tools=[QuizCalculator(), EmailSender()],
             verbose=True
         )
